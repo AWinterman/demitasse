298b332a88886f28f21476a782920ae8138b26a0
AWinterman <andywinterman@gmail.com>
2013-05-14 18:34:51 -0700

    no getting a much better error when i try to run the demo. Going to python

--- example/demo.js
+++ example/demo.js
@@ -6,29 +6,29 @@ demi = new Demi("./docs")
 
 demi.route("^/$", "index.html")
     .on("GET", function(req){
-      return fs.readFileSync("docs/index.html")
+      return fs.readFileSync("../docs/index.html")
     })
 
 demi.route("demi.*")
     .on("GET", function(req){
-      return fs.readFileSync("docs/demi.html")
+      return fs.readFileSync("../docs/demi.html")
     })
 
 demi.route("^/stream_handler*")
     .on("GET", function(req){
-      return fs.readFileSync("docs/stream_handler.html")
+      return fs.readFileSync("../docs/stream_handler.html")
     })
 
 demi.route("/demo*")
     .on("GET", function(req){
-      return fs.readFileSync("docs/demo-app.html")
+      return fs.readFileSync("../docs/demo-app.html")
     })
-
-demi.route(".*\.css")
-    .on("GET", function(req){
-      var parsed = url.parse(req.url, true)
-      return fs.readFileSync("docs/" + parsed.pathname)
-    })
-
+// 
+// demi.route(".*\.css")
+//     .on("GET", function(req){
+//       var parsed = url.parse(req.url, true)
+//       return fs.readFileSync("docs/" + parsed.pathname)
+//     })
+// 
 demi.run(8769)
 
